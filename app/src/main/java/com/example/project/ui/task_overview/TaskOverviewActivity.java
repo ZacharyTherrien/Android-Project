b33fb2a3243aea60e64dc0db78f5b60dc9d45fa5
tonyvas <tony.vasiliev8@gmail.com>
2020-12-03 15:43:34 -0500

    overview works, creates random entries and updates recycler view, ordered alphabetically by entry name

--- app/src/main/java/com/example/project/ui/task_overview/TaskOverviewActivity.java
+++ app/src/main/java/com/example/project/ui/task_overview/TaskOverviewActivity.java
@@ -13,12 +13,27 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project.R;
+import com.example.project.model.Entry;
 import com.example.project.ui.task_entry.TaskEntryActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 public class TaskOverviewActivity extends AppCompatActivity {
+
+    private List<Entry> entries;
+    private EntryViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,14 @@ public class TaskOverviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_overview);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.overview_tasks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        this.entries = new ArrayList<>();
+
+        this.adapter = new EntryViewAdapter(this, this.entries);
+        recyclerView.setAdapter(this.adapter);
 
         //TODO remove later.
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
@@ -48,8 +71,20 @@ public class TaskOverviewActivity extends AppCompatActivity {
     }
 
     private void createRandomEntry(){
-        View taskView = getLayoutInflater().inflate(R.layout.task_overview_item, (ViewGroup) findViewById(R.id.overview_tasks));
-//        ((TextView) taskView.findViewById(R.id.overview_item_title)).setText("text goes here");
-//        ((TextView) taskView.findViewById(R.id.overview_item_time)).setText("text goes here too");
+        Random random = new Random();
+        Entry entry = new Entry();
+
+        entry.setName("name" + (1000 + random.nextInt(9000)));
+        entry.setTime(random.nextInt(1000000));
+
+        entries.add(entry);
+
+        Collections.sort(this.entries, new Comparator<Entry>() {
+            @Override public int compare(Entry a, Entry b) {
+                return a.getName().compareTo(b.getName());
+            }
+        });
+
+        this.adapter.update();
     }
 }
