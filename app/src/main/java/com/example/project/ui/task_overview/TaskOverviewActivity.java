b0a04a247221d30dbad2d50a505a6e34bd3b707a
tonyvas <tony.vasiliev8@gmail.com>
2020-12-02 22:54:00 -0500

    stuff with overview, mostly xml stuff, but also some button stuff

--- app/src/main/java/com/example/project/ui/task_overview/TaskOverviewActivity.java
+++ app/src/main/java/com/example/project/ui/task_overview/TaskOverviewActivity.java
@@ -3,8 +3,12 @@ package com.example.project.ui.task_overview;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +16,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.project.R;
 import com.example.project.ui.task_entry.TaskEntryActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TaskOverviewActivity extends AppCompatActivity {
-
-    //TODO remove later.
-    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +28,28 @@ public class TaskOverviewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //TODO remove later.
-        button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener(){
-
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                createRandomEntry();
+            }
+        });
+
+        findViewById(R.id.button1).setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
                 Activity activity = (Activity) v.getContext();
                 Intent intent = new Intent(activity, TaskEntryActivity.class);
                 activity.startActivityForResult(intent, 1);
+
+                return true;
             }
         });
+    }
+
+    private void createRandomEntry(){
+        View taskView = getLayoutInflater().inflate(R.layout.task_overview_item, (ViewGroup) findViewById(R.id.overview_tasks));
+//        ((TextView) taskView.findViewById(R.id.overview_item_title)).setText("text goes here");
+//        ((TextView) taskView.findViewById(R.id.overview_item_time)).setText("text goes here too");
     }
 }
