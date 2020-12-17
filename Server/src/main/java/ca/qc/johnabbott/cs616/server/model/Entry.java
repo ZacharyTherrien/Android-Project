c905cd053f6b210eb25636b779b8d1645e5e5ed1
Ian Clement <ian.clement@johnabbott.qc.ca>
2020-12-16 23:08:27 -0500

    Added missing getters and setters

--- Server/src/main/java/ca/qc/johnabbott/cs616/server/model/Entry.java
+++ Server/src/main/java/ca/qc/johnabbott/cs616/server/model/Entry.java
@@ -38,7 +38,60 @@ public class Entry {
     @Temporal(TemporalType.TIMESTAMP)
     private Date endedOn;
 
+    public String getUuid() {
+        return uuid;
+    }
 
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public Date getAddedOn() {
+        return addedOn;
+    }
+
+    public void setAddedOn(Date addedOn) {
+        this.addedOn = addedOn;
+    }
+
+    public Date getStartedOn() {
+        return startedOn;
+    }
+
+    public void setStartedOn(Date startedOn) {
+        this.startedOn = startedOn;
+    }
+
+    public Date getEndedOn() {
+        return endedOn;
+    }
+
+    public void setEndedOn(Date endedOn) {
+        this.endedOn = endedOn;
+    }
 }
 
